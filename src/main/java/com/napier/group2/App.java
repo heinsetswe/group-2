5ef11efd9dcaa99407bcd55bbdedb0dbedc62fe3
40478640 <40478640@live.napier.ac.uk>
2020-11-12 17:40:13 +0630

    display country by population

--- src/main/java/com/napier/group2/App.java
+++ src/main/java/com/napier/group2/App.java
@@ -1,10 +1,19 @@
 package com.napier.group2;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class App
 {
-    public static void main(String[] args)
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
+
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect()
     {
         try
         {
@@ -17,9 +26,7 @@ public class App
             System.exit(-1);
         }
 
-        // Connection to the database
-        Connection con = null;
-        int retries = 100;
+        int retries = 10;
         for (int i = 0; i < retries; ++i)
         {
             System.out.println("Connecting to database...");
@@ -30,9 +37,6 @@ public class App
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
-                // Wait a bit
-                Thread.sleep(10000);
-                // Exit for loop
                 break;
             }
             catch (SQLException sqle)
@@ -45,7 +49,56 @@ public class App
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
+    }
 
+    public ArrayList<Country> getCountry()
+    {
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT Code, Name, Continent, Region, Population, Capital FROM country order by Population DESC";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new country if valid.
+            // Check one is returned
+            ArrayList<Country> ctylist = new ArrayList<Country>();
+            while (rset.next()) {
+                Country cty = new Country();
+                cty.Code = rset.getString("Code");
+                cty.Name = rset.getString("Name");
+                cty.Continent = rset.getString("Continent");
+                cty.Region = rset.getString("Region");
+                cty.Population = rset.getInt("Population");
+                cty.Capital = rset.getInt("Capital");
+                ctylist.add(cty);
+            }
+            return ctylist;
+
+        } catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get country details");
+            return null;
+        }
+    }
+
+    public void displayCountrybyPopu(ArrayList<Country> ctylist)
+    {
+        System.out.println(String.format("%-5s %-30s %-15s %-30s %-25s %-10s","Code", "Name", "Continent", "Region", "Population", "Capital"));
+        System.out.println(String.format("%-5s %-30s %-15s %-30s %-25s %-10s","~~~~", "~~~~", "~~~~~~~~~", "~~~~~~", "~~~~~~~~~~", "~~~~~~~"));
+        for ( Country coty : ctylist)
+        {
+             System.out.println(String.format("%-5s %-30s %-15s %-30s %-25s %-10s", coty.Code, coty.Name, coty.Continent, coty.Region, coty.Population, coty.Capital));
+        }
+    }
+
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
         if (con != null)
         {
             try
@@ -58,5 +111,23 @@ public class App
                 System.out.println("Error closing connection to database");
             }
         }
+    }
+
+    public static void main(String[] args)
+    {
+        // Create new Application
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+
+        // Get countries
+        ArrayList<Country> cty = a.getCountry();
+
+        // Display countries
+        a.displayCountrybyPopu(cty);
+
+        // Disconnect from database
+        a.disconnect();
     }
 }
