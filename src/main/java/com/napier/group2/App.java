8dc2e6ad26ab18a7571879034f948a1ebf379802
40478631 <40478631@live.napier.ac.uk>
2020-11-13 10:40:04 +0630

    country output fixing

--- src/main/java/com/napier/group2/App.java
+++ src/main/java/com/napier/group2/App.java
@@ -125,6 +125,8 @@ public class App
         for ( Country coty : ctylist)
         {
              System.out.println(String.format("%-5s %-30s %-15s %-30s %-25s %-10s", coty.Code, coty.Name, coty.Continent, coty.Region, coty.Population, coty.Capital));
+            System.out.println("===================================================================================================");
+            System.out.println("\n");
         }
     }
 
@@ -136,6 +138,8 @@ public class App
         for ( Country coty : ctylistcon)
         {
             System.out.println(String.format("%-5s %-30s %-15s %-30s %-25s %-10s", coty.Code, coty.Name, coty.Continent, coty.Region, coty.Population, coty.Capital));
+            System.out.println("===================================================================================================");
+            System.out.println("\n");
         }
     }
 
@@ -181,6 +185,8 @@ public class App
         for ( Country coty : ctylistreg)
         {
             System.out.println(String.format("%-5s %-30s %-15s %-30s %-25s %-10s", coty.Code, coty.Name, coty.Continent, coty.Region, coty.Population, coty.Capital));
+            System.out.println("===================================================================================================");
+            System.out.println("\n");
         }
     }
 
