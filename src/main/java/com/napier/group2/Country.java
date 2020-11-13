7d0566d801e88c3a98b7c466c1827a6288d18cdb
40478631 <40478631@live.napier.ac.uk>
2020-11-13 10:43:08 +0630

    country output fixing 1

---
+++ src/main/java/com/napier/group2/Country.java
@@ -0,0 +1,80 @@
+package com.napier.group2;
+
+import java.text.DecimalFormat;
+
+public class Country {
+    /**
+     * Country code
+     */
+    public String Code;
+
+    /**
+     * Country name
+     */
+    public String Name;
+
+    /**
+     * Continent of country
+     */
+    public String Continent;
+
+    /**
+     * Region of country
+     */
+    public String Region;
+
+    /**
+     * SurfaceArea of country
+     */
+    public Float SurfaceArea;
+
+    /**
+     * Independent year
+     */
+    public int IndepYear;
+
+    /**
+     * Population of country
+     */
+    public int Population;
+
+    /**
+     * LifeExpectancy of country
+     */
+    public float LifeExpectancy;
+
+    /**
+     * GNP of country
+     */
+    public Float GNP;
+
+    /**
+     * GNPOld of country
+     */
+    public Float GNPOld;
+
+    /**
+     * LocalName of country
+     */
+    public String LocalName;
+
+    /**
+     * GovernmentForm of country
+     */
+    public int GovernmentForm;
+
+    /**
+     * Head of State
+     */
+    public String HeadOfState;
+
+    /**
+     * Capital of country
+     */
+    public int Capital;
+
+    /**
+     * Country's code-2
+     */
+    public String Code2;
+}
