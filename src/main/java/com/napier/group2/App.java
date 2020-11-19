2730084e424bdcb45dd3e2db5e1b8180798da384
40478640 <40478640@live.napier.ac.uk>
2020-11-19 11:35:26 +0630

    get set functions used

--- src/main/java/com/napier/group2/App.java
+++ src/main/java/com/napier/group2/App.java
@@ -66,12 +66,12 @@ public class App
             ArrayList<Country> ctylist = new ArrayList<Country>();
             while (rset.next()) {
                 Country cty = new Country();
-                cty.Code = rset.getString("Code");
-                cty.Name = rset.getString("Name");
-                cty.Continent = rset.getString("Continent");
-                cty.Region = rset.getString("Region");
-                cty.Population = rset.getInt("Population");
-                cty.Capital = rset.getInt("Capital");
+                cty.setCode(rset.getString("Code"));
+                cty.setName(rset.getString("Name"));
+                cty.setContinent(rset.getString("Continent"));
+                cty.setRegion(rset.getString("Region"));
+                cty.setPopulation(rset.getInt("Population"));
+                cty.setCapital(rset.getInt("Capital"));
                 ctylist.add(cty);
             }
             return ctylist;
@@ -98,14 +98,14 @@ public class App
             // Check one is returned
             ArrayList<Country> ctylistcon = new ArrayList<Country>();
             while (rset.next()) {
-                Country ctycon = new Country();
-                ctycon.Code = rset.getString("Code");
-                ctycon.Name = rset.getString("Name");
-                ctycon.Continent = rset.getString("Continent");
-                ctycon.Region = rset.getString("Region");
-                ctycon.Population = rset.getInt("Population");
-                ctycon.Capital = rset.getInt("Capital");
-                ctylistcon.add(ctycon);
+                Country cty = new Country();
+                cty.setCode(rset.getString("Code"));
+                cty.setName(rset.getString("Name"));
+                cty.setContinent(rset.getString("Continent"));
+                cty.setRegion(rset.getString("Region"));
+                cty.setPopulation(rset.getInt("Population"));
+                cty.setCapital(rset.getInt("Capital"));
+                ctylistcon.add(cty);
             }
             return ctylistcon;
 
@@ -115,32 +115,6 @@ public class App
             System.out.println("Failed to get country details");
             return null;
         }
-    }
-
-    public void displayCountrybyPopu(ArrayList<Country> ctylist)
-    {
-        System.out.println("All the countries in the world organised by largest population to smallest.");
-        System.out.println(String.format("%-5s %-30s %-15s %-30s %-25s %-10s","Code", "Name", "Continent", "Region", "Population", "Capital"));
-        System.out.println(String.format("%-5s %-30s %-15s %-30s %-25s %-10s","~~~~", "~~~~", "~~~~~~~~~", "~~~~~~", "~~~~~~~~~~", "~~~~~~~"));
-        for ( Country coty : ctylist)
-        {
-             System.out.println(String.format("%-5s %-30s %-15s %-30s %-25s %-10s", coty.Code, coty.Name, coty.Continent, coty.Region, coty.Population, coty.Capital));
-        }
-        System.out.println("===================================================================================================");
-        System.out.println("\n");
-    }
-
-    public void displayCountrycon(ArrayList<Country> ctylistcon)
-    {
-        System.out.println("All the countries in a continent organised by largest population to smallest.");
-        System.out.println(String.format("%-5s %-30s %-15s %-30s %-25s %-10s","Code", "Name", "Continent", "Region", "Population", "Capital"));
-        System.out.println(String.format("%-5s %-30s %-15s %-30s %-25s %-10s","~~~~", "~~~~", "~~~~~~~~~", "~~~~~~", "~~~~~~~~~~", "~~~~~~~"));
-        for ( Country coty : ctylistcon)
-        {
-            System.out.println(String.format("%-5s %-30s %-15s %-30s %-25s %-10s", coty.Code, coty.Name, coty.Continent, coty.Region, coty.Population, coty.Capital));
-        }
-        System.out.println("===================================================================================================");
-        System.out.println("\n");
     }
 
     //**All the countries in a region organised by largest population to smallest.**//
@@ -158,14 +132,14 @@ public class App
             // Check one is returned
             ArrayList<Country> ctylistreg = new ArrayList<Country>();
             while (rset.next()) {
-                Country ctyreg = new Country();
-                ctyreg.Code = rset.getString("Code");
-                ctyreg.Name = rset.getString("Name");
-                ctyreg.Continent = rset.getString("Continent");
-                ctyreg.Region = rset.getString("Region");
-                ctyreg.Population = rset.getInt("Population");
-                ctyreg.Capital = rset.getInt("Capital");
-                ctylistreg.add(ctyreg);
+                Country cty = new Country();
+                cty.setCode(rset.getString("Code"));
+                cty.setName(rset.getString("Name"));
+                cty.setContinent(rset.getString("Continent"));
+                cty.setRegion(rset.getString("Region"));
+                cty.setPopulation(rset.getInt("Population"));
+                cty.setCapital(rset.getInt("Capital"));
+                ctylistreg.add(cty);
             }
             return ctylistreg;
 
@@ -177,16 +151,22 @@ public class App
         }
     }
 
-    public void displayCountryreg(ArrayList<Country> ctylistreg)
+    public void displayCountry(ArrayList<Country> countrytylist)
     {
         System.out.println("All the countries in the world organised by largest population to smallest.");
         System.out.println(String.format("%-5s %-30s %-15s %-30s %-25s %-10s","Code", "Name", "Continent", "Region", "Population", "Capital"));
         System.out.println(String.format("%-5s %-30s %-15s %-30s %-25s %-10s","~~~~", "~~~~", "~~~~~~~~~", "~~~~~~", "~~~~~~~~~~", "~~~~~~~"));
-        for ( Country coty : ctylistreg)
+        for ( Country coty : countrytylist)
         {
-            System.out.println(String.format("%-5s %-30s %-15s %-30s %-25s %-10s", coty.Code, coty.Name, coty.Continent, coty.Region, coty.Population, coty.Capital));
+            String Code = coty.getCode();
+            String Name = coty.getName();
+            String Continent = coty.getContinent();
+            String Region = coty.getRegion();
+            int Population = coty.getPopulation();
+            int Capital = coty.getCapital();
+            System.out.println(String.format("%-5s %-30s %-15s %-30s %-25s %-10s", Code, Name, Continent, Region, Population, Capital));
         }
-        System.out.println("===================================================================================================");
+        System.out.println("=========================================================================================================");
         System.out.println("\n");
     }
 
@@ -221,10 +201,11 @@ public class App
         ArrayList<Country> cty = a.getCountrybyPopu();
         ArrayList<Country> ctycon = a.getCountrycon();
         ArrayList<Country> ctyreg = a.getCountryreg();
+
         // Display countries
-        a.displayCountrybyPopu(cty);
-        a.displayCountrycon(ctycon);
-        a.displayCountryreg(ctyreg);
+        a.displayCountry(cty);
+        a.displayCountry(ctycon);
+        a.displayCountry(ctyreg);
 
         // Disconnect from database
         a.disconnect();
--- src/main/java/com/napier/group2/Country.java
+++ src/main/java/com/napier/group2/Country.java
@@ -6,75 +6,165 @@ public class Country {
     /**
      * Country code
      */
-    public String Code;
+    private String Code;
+    public String getCode(){
+        return Code;
+    }
+    public void setCode(String attribute){
+        this.Code = attribute;
+    }
 
     /**
      * Country name
      */
-    public String Name;
+    private String Name;
+    public String getName(){
+        return Name;
+    }
+    public void setName(String attribute){
+        this.Name = attribute;
+    }
 
     /**
      * Continent of country
      */
-    public String Continent;
+    private String Continent;
+    public String getContinent(){
+        return Continent;
+    }
+    public void setContinent(String attribute){
+        this.Continent = attribute;
+    }
 
     /**
      * Region of country
      */
-    public String Region;
+    private String Region;
+    public String getRegion(){
+        return Region;
+    }
+    public void setRegion(String attribute){
+        this.Region = attribute;
+    }
 
     /**
      * SurfaceArea of country
      */
-    public Float SurfaceArea;
+    private Float SurfaceArea;
+    public Float getSurfaceArea(){
+        return SurfaceArea;
+    }
+    public void setSurfaceArea(Float attribute){
+        this.SurfaceArea = attribute;
+    }
 
     /**
      * Independent year
      */
-    public int IndepYear;
+    private int IndepYear;
+    public int getIndepYear(){
+        return IndepYear;
+    }
+    public void setIndepYear(int attribute){
+        this.IndepYear = attribute;
+    }
 
     /**
      * Population of country
      */
-    public int Population;
+    private int Population;
+    public int getPopulation(){
+        return Population;
+    }
+    public void setPopulation(int attribute){
+        this.Population = attribute;
+    }
 
     /**
      * LifeExpectancy of country
      */
-    public float LifeExpectancy;
+    private float LifeExpectancy;
+    public float getLifeExpectancy(){
+        return LifeExpectancy;
+    }
+    public void setLifeExpectancy(float attribute){
+        this.LifeExpectancy = attribute;
+    }
 
     /**
      * GNP of country
      */
-    public Float GNP;
+    private float GNP;
+    public float getGNP(){
+        return GNP;
+    }
+    public void setGNP(float attribute){
+        this.GNP = attribute;
+    }
 
     /**
      * GNPOld of country
      */
-    public Float GNPOld;
+    private float GNPOld;
+    public float getGNPOld(){
+        return GNPOld;
+    }
+    public void setGNPOld(float attribute){
+        this.GNPOld = attribute;
+    }
 
     /**
      * LocalName of country
      */
-    public String LocalName;
+    private String LocalName;
+    public String getLocalName(){
+        return LocalName;
+    }
+    public void setLocalName(String attribute){
+        this.LocalName = attribute;
+    }
 
     /**
      * GovernmentForm of country
      */
-    public int GovernmentForm;
+    private int GovernmentForm;
+    public int getGovernmentForm(){
+        return GovernmentForm;
+    }
+    public void setGovernmentForm(int attribute){
+        this.GovernmentForm = attribute;
+    }
 
     /**
      * Head of State
      */
-    public String HeadOfState;
+    private String HeadOfState;
+    public String getHeadOfState(){
+        return HeadOfState;
+    }
+    public void setHeadOfState(String attribute){
+        this.HeadOfState = attribute;
+    }
 
     /**
      * Capital of country
      */
-    public int Capital;
+    private int Capital;
+    public int getCapital(){
+        return Capital;
+    }
+    public void setCapital(int attribute){
+        this.Capital = attribute;
+    }
 
     /**
      * Country's code-2
      */
-    public String Code2;
+    private String Code2;
+    public String getCode2(){
+        return Code2;
+    }
+    public void setCode2(String attribute){
+        this.Code2 = attribute;
+    }
 }
