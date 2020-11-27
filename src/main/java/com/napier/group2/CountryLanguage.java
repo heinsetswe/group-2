5f44d729066ef65cebb50b3f4b4ce477d25e2856
40478640 <40478640@live.napier.ac.uk>
2020-11-27 10:37:29 +0630

    CountryLanguage.java file updated

--- src/main/java/com/napier/group2/CountryLanguage.java
+++ src/main/java/com/napier/group2/CountryLanguage.java
@@ -4,21 +4,33 @@ public class CountryLanguage {
     /**
      * Country code
      */
-    public String CountryCode;
+    private String CountryCode;
+    public String getCountryCode(){
+        return CountryCode;
+    }
+    public void setCountryCode(String attribute){
+        this.CountryCode = attribute;
+    }
 
     /**
      * Language
      */
-    public String Language;
+    private String Language;
+    public String getLanguage() { return Language; }
+    public void setLanguage(String attribute) { this.Language = attribute; }
 
     /**
      * IsOfficial of language
      */
-    public String IsOfficial;
+    private String IsOfficial;
+    public String getIsOfficial() { return IsOfficial; }
+    public void setIsOfficial(String attribute) { this.IsOfficial = attribute; }
 
     /**
      * Percentage of people
      */
-    public Float Percentage;
+    private Float Percentage;
+    public Float getPercentage() { return Percentage; }
+    public void setPercentage(Float attribute) { this.Percentage = attribute; }
 
 }
